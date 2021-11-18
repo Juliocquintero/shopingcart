7af11fe48b1169758f0abf55ebbe085c1ee8fb46
Julioq <juliocquinterog17@gmail.com>
2021-11-17 21:31:46 -0300

    first version 11-17-2021

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './assets/styles/App.css';
+import CartBar from './components/cartBar';
+import Header from './components/header';
+import ModalDetail from './components/modalDetail/modalDetail';
+import ProductList from './components/productList/productList';
+import { TYPES } from './redux/actions';
 
-function App() {
+const App = () => {
+  const products = useSelector((state) => state.products);
+  const cart = useSelector((state) => state.cart);
+  const totalInCart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const [showCart, setShowCart] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleToggleMenu = () => {
+    if (showCart) {
+      setShowCart(false);
+    } else {
+      setShowCart(true);
+    }
+  };
+  const handleShowModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const addToCart = (id) => {
+    dispatch({ type: TYPES.ADD_TO_CART, payload: id });
+  };
+
+  const addOneToCart = (id) => {
+    dispatch({ type: TYPES.ADD_ONE_FROM_CART, payload: id });
+  };
+  const removeOneFromCart = (id) => {
+    dispatch({ type: TYPES.REMOVE_ONE_FROM_CART, payload: id });
+  };
+  const removeAllFromCart = (id) => {
+    dispatch({ type: TYPES.REMOVE_ALL_FROM_CART, payload: id });
+  };
+  const clearCart = () => {
+    dispatch({ type: TYPES.RESET_CART });
+  };
+  const handleContentViewed = (data = {}) => {
+    dispatch({ type: TYPES.SET_CONTENT_VIEWED, payload: data });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header cart={cart} handleToggleMenu={handleToggleMenu} />
+      <CartBar
+        cart={cart}
+        handleToggleMenu={handleToggleMenu}
+        removeOneFromCart={removeOneFromCart}
+        addOneToCart={addOneToCart}
+        removeAllFromCart={removeAllFromCart}
+        clearCart={clearCart}
+        totalInCart={totalInCart}
+        showCart={showCart}
+      />
+
+      <h1 className="products-title">Productos</h1>
+      <ProductList
+        products={products}
+        addToCart={addToCart}
+        handleContentViewed={handleContentViewed}
+        handleShowModal={handleShowModal}
+      />
+      <ModalDetail showModal={showModal} handleShowModal={handleShowModal} addToCart={addToCart} />
+    </>
   );
-}
+};
 
 export default App;
